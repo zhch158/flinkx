756f4204108a2d2496a28bdaf5d33fbdacfe8d51
zhengchun <zhengchun@yusys.com.cn>
2020-03-24 11:50:40 +0800

    modified chg_dot_dir.sh

--- zhch/chg_dot_dir.sh
+++ zhch/chg_dot_dir.sh
@@ -1,18 +1,28 @@
-if [ $# = 0 ]
+if [ $# != 1 ]
 then
 	echo "Usage: $0 directory"
 	exit 1
 fi
-for dir in $*
+
+cur_dir=$PWD
+for s_dir in `find $1 -path .metadata -prune -o -type d -name "*.*.*.*" -print`
 do
+	cd $cur_dir
 	echo ""
+	dir=${s_dir##*\/}
+	p_dir=${s_dir%\/*}
+	echo "[$p_dir]-->[$dir]"
+	cd $p_dir
+
 	fulldir=${dir//\./\/}
 	cmd1="mkdir -p $fulldir"
 	echo "[$dir]-->$cmd1"
 	eval $cmd1
+
 	cmd2="cp $dir/* $fulldir"
 	echo "$cmd2"
 	eval $cmd2
+
 	cmd3="rm -rf $dir"
 	echo "$cmd3"
 	eval $cmd3
